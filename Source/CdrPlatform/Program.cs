297334e75977d6a280953728635e738aa65c2aa5
BarbTheOnlyOne <michal.barborik96@gmail.com>
2025-04-06 12:29:18 +0200

    Add database to the builder, ensure it exists

--- Source/CdrPlatform/Program.cs
+++ Source/CdrPlatform/Program.cs
@@ -1,6 +1,20 @@
+using CdrPlatform.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CdrDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetSection("ConnectionStrings:DefaultConnection").Value));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// Ensure it exists somewhere, not ideal as migrations apparently
+using (var scope = app.Services.CreateAsyncScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CdrDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.Run();
