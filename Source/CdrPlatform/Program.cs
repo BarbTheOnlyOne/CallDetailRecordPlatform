749319684a4bc027ac9915b4295bc18d2977aeb8
BarbTheOnlyOne <michal.barborik96@gmail.com>
2025-04-06 12:37:16 +0200

    Add testing endpoints

--- Source/CdrPlatform/Program.cs
+++ Source/CdrPlatform/Program.cs
@@ -1,4 +1,6 @@
+using CdrPlatform.Abstractions;
 using CdrPlatform.Database;
+using CdrPlatform.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,33 @@ builder.Services.AddDbContext<CdrDbContext>(options =>
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbtest/insert", async (CdrDbContext context) =>
+{
+    var testRecord = new CallDetailRecord
+    {
+        CallerId = 1234567890,
+        Recipient = 987456321,
+        CallDate = DateOnly.FromDateTime(DateTime.Now),
+        EndTime = TimeSpan.FromMinutes(12),
+        Duration = 42,
+        Cost = 20,
+        Reference = "TestReference",
+        Currency = Currency.GBP
+    };
+    
+    context.CallDetailRecords.Add(testRecord);
+    
+    await context.SaveChangesAsync();
+    
+    return Results.Ok("Inserted test record");
+});
+
+app.MapGet("/dbtest/get", async (CdrDbContext context) =>
+{
+    var records = await context.CallDetailRecords.ToListAsync();
+    return Results.Ok(records);
+});
 
 // Ensure it exists somewhere, not ideal as migrations apparently
 using (var scope = app.Services.CreateAsyncScope())
