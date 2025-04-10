dd7b7a044b10b078dad8238a4b70d5ca8cac77c4
BarbTheOnlyOne <michal.barborik96@gmail.com>
2025-04-10 12:04:39 +0200

    Fix parsing of DateOnly

--- Source/CdrPlatform/Services/CallDetailRecordMap.cs
+++ Source/CdrPlatform/Services/CallDetailRecordMap.cs
@@ -10,7 +10,7 @@ public sealed class CallDetailRecordMap : ClassMap<CallDetailRecord>
     {
         Map(m => m.CallerId).Name("caller_id");
         Map(m => m.Recipient).Name("recipient");
-        Map(m => m.CallDate).Name("call_date").TypeConverterOption.Format("yyyy/MM/dd");
+        Map(m => m.CallDate).Name("call_date").TypeConverterOption.Format("dd/MM/yyyy");
         Map(m => m.EndTime).Name("end_time").TypeConverterOption.Format("HH:mm:ss");
         Map(m => m.Duration).Name("duration");
         Map(m => m.Cost).Name("cost");
